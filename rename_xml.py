91b689cdb9fe26a1c02320938b23d4a522f755ce
Zhuogang <pengzg698@gmail.com>
2022-01-29 21:14:13 -0500

    add images

--- rename_xml.py
+++ rename_xml.py
@@ -1,7 +1,5 @@
 import os
 import glob
-import cv2
-from pathlib import Path
 
 if __name__ == "__main__":
     import argparse
@@ -22,38 +20,30 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument(
-        "--ext", help="Raw image files extension to resize.", default="jpg", type=str
+        "--ext", help="Raw image files extension to resize.", default="xml", type=str
     )
-    parser.add_argument(
-        "--target-size",
-        help="Target size to resize as a tuple of 2 integers.",
-        default="(800, 600)",
-        type=str,
-    )
+
+
     args = parser.parse_args()
 
     raw_dir = args.raw_dir
     save_dir = args.save_dir
     ext = args.ext
-    target_size = eval(args.target_size)
-    msg = "--target-size must be a tuple of 2 integers"
-    assert isinstance(target_size, tuple) and len(target_size) == 2, msg
+
     fnames = glob.glob(os.path.join(raw_dir, "*.{}".format(ext)))
     os.makedirs(save_dir, exist_ok=True)
     print(
-        "{} files to resize from directory `{}` to target size:{}".format(
-            len(fnames), raw_dir, target_size
+        "{} files to rename from directory `{}`".format(
+            len(fnames), raw_dir
         )
     )
+
     for i, fname in enumerate(fnames):
-        # print(os.path.splitext(fname)[0])        
-        filename = Path(fname).stem
         print(".", end="", flush=True)
-        img = cv2.imread(fname)
-        img_small = cv2.resize(img, target_size)
-        new_fname = "{}.{}".format(filename, ext)
+        new_fname = "{}.{}".format(str(i), ext)
         small_fname = os.path.join(save_dir, new_fname)
-        cv2.imwrite(small_fname, img_small)
+        os.rename(fname, new_fname)
+
     print(
         "\nDone resizing {} files.\nSaved to directory: `{}`".format(
             len(fnames), save_dir
